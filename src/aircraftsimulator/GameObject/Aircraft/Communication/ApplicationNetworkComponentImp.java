3d2704f851e2ee0335292779f04c2b2af11f2ad1
Keita Nakashima <tun05036@temple.edu>
2024-06-15 21:55:12 +0900

    resend condition revised and supre ssed timeout print

--- src/aircraftsimulator/GameObject/Aircraft/Communication/ApplicationNetworkComponentImp.java
+++ src/aircraftsimulator/GameObject/Aircraft/Communication/ApplicationNetworkComponentImp.java
@@ -8,6 +8,7 @@ public class ApplicationNetworkComponentImp extends NetworkComponentImp implemen
 
     private Map<String, ResendPacket> sentPacketMap;
     private final Map<String, Integer> resentNumberMap;
+    private final Map<String, Packet> lastSentPacketMap;
 
     private final int resentThreshold;
     private final long timeout;
@@ -23,6 +24,7 @@ public class ApplicationNetworkComponentImp extends NetworkComponentImp implemen
     {
         super(network, updateInterval);
         resentNumberMap = new HashMap<>();
+        lastSentPacketMap = new HashMap<>();
 
         this.timeout = timeout;
         this.resentThreshold = resentThreshold;
@@ -33,19 +35,35 @@ public class ApplicationNetworkComponentImp extends NetworkComponentImp implemen
         if(!sessionManager.isTimeout(sessionId, timeout))
             resentNumberMap.remove(sessionId);
 
-        if(!sessionManager.isTimeout(sessionId, timeout * resentNumberMap.getOrDefault(sessionId, 1)))
+        if(!sessionManager.isTimeout(sessionId, timeout * (resentNumberMap.getOrDefault(sessionId, 0) + 1)))
             return;
         else
-            resentNumberMap.put(sessionId, resentNumberMap.getOrDefault(sessionId, 1) + 1);
+            resentNumberMap.put(sessionId, resentNumberMap.getOrDefault(sessionId, 0) + 1);
 
-        System.out.println("Resending");
+        SessionInformation info = sessionManager.getSessionInformation(sessionId);
 
         if(resentNumberMap.get(sessionId) > resentThreshold)
         {
             int port = sessionManager.getSessionInformation(sessionId).sourcePort();
             releasePort(port);
             resentNumberMap.remove(sessionId);
+            return;
         }
+
+        if(lastSentPacketMap.containsKey(sessionId))
+        {
+            send(lastSentPacketMap.get(sessionId));
+            System.out.printf("[%6s-%6s] Port [%d] Resent last packet\n", getMac().substring(0, 6), info.destinationMac().substring(0, 6), info.sourcePort());
+        }
+        else{
+            System.out.printf("[%6s-%6s] Port [%d] Failed to resent last packet\n", getMac().substring(0, 6), info.destinationMac().substring(0, 6), info.sourcePort());
+        }
+    }
+
+    @Override
+    public void send(Packet packet) {
+        super.send(packet);
+        lastSentPacketMap.put(packet.getSessionID(), packet);
     }
 
     public void setResendLimit(Integer limit) {
@@ -64,7 +82,6 @@ public class ApplicationNetworkComponentImp extends NetworkComponentImp implemen
     public void triggerTimeout(Integer port, SessionInformation sessionInformation) {
         if(portStateMap.get(port) == PortState.CONNECTING) {
 //            changePortState(port, PortState.OPEN, sessionManager.getSessionId(port), sessionInformation.destinationPort(), sessionInformation.destinationMac());
-            System.out.printf("[%6s-] Port [%d] timeout\n", getMac().substring(0, 6), port);
 //            sessionManager.updateSession(sessionManager.getSessionId(port));
             resendData(sessionManager.getSessionId(port));
         }
